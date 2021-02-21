af0c5c09fddb5d1197d2bfaee3d78041eaa9464b
Labrege <antoine.brege92@gmail.com>
2021-02-21 17:26:19 +0100

    modif front

--- espace-membre/espace_membre_informations.php
+++ espace-membre/espace_membre_informations.php
@@ -25,10 +25,39 @@ $sql = $conn->query("SELECT * FROM users WHERE userUid = '$user");
     <button type="submit" name="submit"> Changer mon mot de passe </button>
 </form>
 
+<form>
+    <div class="user-form">
+        <div class="ligne ">
+            <label class="material-input__label" for="firstname">Prénom</label>
+            <input name="firstname" class="material-input__input" type="text" id="firstname" placeholder="John" value=<?php echo $_SESSION["username"]; ?>>
+        </div>
 
+        <div class="ligne ">
+            <label class="material-input__label" for="lastname">Nom</label>
+            <input name="lastname" class="material-input__input" type="text" id="lastname" placeholder="Dupont" value=<?php echo $_SESSION["usersurname"]; ?>>
+        </div>
+        <div class="ligne ">
+            <label class="material-input__label" for="useruid">Nom d'utilisateur</label>
+            <input name="useruid" class="material-input__input" type="text" id="useruid" placeholder="John Dupont" value=<?php echo $_SESSION["useruid"]; ?>>
+        </div>
 
+        <div class="ligne ">
+            <label class="material-input__label" for="email">Email</label>
+            <input name="email" class="material-input__input" type="text" id="email" placeholder="John" value=<?php echo $_SESSION["useremail"]; ?>>
+        </div>
 
+        <div class="ligne ">
+            <label class="material-input__label" for="firstname">Date de naissance</label>
+            <input name="birthdate" class="material-input__input" type="date" id="birthdate" placeholder="2000-01-01" value=>
+        </div>
 
+        <div class="ligne ">
+            <label class="material-input__label" for="Club">Club </label>
+            <input name="Club" class="material-input__input" type="text" id="Club" placeholder="OvalXV" value=>
+        </div>
+
+    </div>
+</div>
 
 
 
