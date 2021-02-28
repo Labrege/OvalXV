4ad0301e9d6ecd7821017553529500595c2f6083
Labrege <antoine.brege92@gmail.com>
2021-02-28 01:39:25 +0100

    espace membre abonnement
    mise en place html et debut css

--- espace-membre/espace_membre_abonnement.php
+++ espace-membre/espace_membre_abonnement.php
@@ -17,3 +17,70 @@ require_once '../espace-membre/espace_membre_header.php';
 </ul>
 
 
+<div class="offre_abonement_global">
+    <div class="level_plan" >
+        <div class="plan_gratuit">
+            <div class="titre">
+                   <h3>Essaie</h3>
+                   <img src="../Images/icons-abonement-bronze.png" alt="">
+            </div>
+            
+            <div class="contenue_offre">
+                <div class="prix par mois">Gratuit</div>
+                <div class="prix par an"></div>
+                <div class="contenue_propose">
+                    <ul class="liste_contenu_plan">
+                        <li class="contenu_plan"><img src="../Images/tick.png" alt=""> acces a 20 videos </li>
+                        <li class="contenu_plan"> <img src="../Images/tick.png" alt="">nombre d'entrainement disponible 5 </li>
+                        <li class="contenu_plan"> <img src="../Images/tick.png" alt="">2 appareils pour un seul compte </li>
+                    </ul>
+                </div>
+            </div>
+            <a href="" class="bouton_valider">Choisir ce plan</a>
+        </div>
+
+
+        <div class="plan_prenium">
+            <div class="titre">
+                   <h3>prenium</h3>
+                   <img src="../Images/icons-abonement-argent.png" alt="">
+            </div>
+            
+            <div class="contenue_offre">
+                <div class="prix par mois">10€ /mois</div>
+                <div class="prix par an">offre annuel: 100€/ans</div>
+                <div class="contenue_propose">
+                    <ul class="liste_contenu_plan">
+                        <li class="contenu_plan"><img src="../Images/tick.png" alt=""> acces a 20 videos </li>
+                        <li class="contenu_plan"> <img src="../Images/tick.png" alt="">nombre d'entrainement disponible 5 </li>
+                        <li class="contenu_plan"> <img src="../Images/tick.png" alt="">2 appareils pour un seul compte </li>
+                    </ul>
+                </div>
+            </div>
+            <a href="" class="bouton_valider">Choisir ce plan</a>
+        </div>
+
+        <div class="plan_club">
+            <div class="titre">
+                   <h3>Club</h3>
+                   <img src="../Images/icons-abonement-or.png" alt="">
+            </div>
+            
+            <div class="contenue_offre">
+                <div class="prix par mois">25€</div>
+                <div class="prix par an">offre annuel: 250€/ans</div>
+                <div class="contenue_propose">
+                    <ul class="liste_contenu_plan">
+                        <li class="contenu_plan"><img src="../Images/tick.png" alt=""> acces a 20 videos </li>
+                        <li class="contenu_plan"> <img src="../Images/tick.png" alt="">nombre d'entrainement disponible 5 </li>
+                        <li class="contenu_plan"> <img src="../Images/tick.png" alt="">2 appareils pour un seul compte </li>
+                    </ul>
+                </div>
+            </div>
+            <a href="" class="bouton_valider">Choisir ce plan</a>
+        </div>
+
+    </div>
+</div>
+
+
