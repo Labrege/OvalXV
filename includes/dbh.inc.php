d44f17f131ba3ad555e43480f21152407708202e
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-08-16 14:49:35 +0200

    h

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,4 +1,16 @@
 <?php
+/*require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/dbh.inc.php');
+$servername = "localhost";
+$dbusername = "u519972031_solal";
+$dbpassword = "OvalXV75016";
+$dbname = "u519972031_DB";
+
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+if(!$conn){
+    die("Connection failed: ". mysqli_connect_error());
+}
+*/
 
 $servername = "localhost";
 $dbusername = "root";
